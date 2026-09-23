cda085bcc1354f8ae62a3c46077cbbd65b29f039
sirazet <sirazetdinovarslan@gmail.com>
2026-09-23 12:43:28 +0300

    Добавлены отдельные статические методы и if заменен на switch

--- 1 year/Программирование/Лабораторная 1/Lab1.java
+++ 1 year/Программирование/Лабораторная 1/Lab1.java
@@ -1,4 +1,21 @@
 public class Lab1 {
+    public static double calculate(short ni, double xj) {
+        return switch (ni) {
+            case 1 -> Math.sin(Math.pow(Math.E, xj));
+            case 3, 5, 7, 9, 10, 11, 12, 15, 18 -> Math.sin(Math.asin(Math.cos(xj)));
+            default -> Math.asin(Math.pow(Math.pow(Math.cos(xj), 2), 2));
+        };
+    }
+
+    public static void printMatrix(double[][] matrix) {
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 12; j++) {
+                System.out.printf("%7.3f", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         short[] n = new short[19];
         for (int i = 0; i < 19; i++) {
@@ -13,22 +30,10 @@ public class Lab1 {
         double[][] d = new double[19][12];
         for (int i = 0; i < 19; i++) {
             for (int j = 0; j < 12; j++) {
-                if (n[i] == 1) {
-                    d[i][j] = Math.sin(Math.pow(Math.E, x[j]));
-                }
-                else if (java.util.List.of(3, 5, 7, 9, 10, 11, 12, 15, 18).contains((int) n[i])) {
-                    d[i][j] = Math.sin(Math.asin(Math.cos(x[j])));
-                }
-                else {
-                    d[i][j] = Math.asin(Math.pow(Math.pow(Math.cos(x[j]), 2), 2));
-                }
+                d[i][j] = calculate(n[i], x[j]);
             }
         }
-        for (int i = 0; i < 19; i++) {
-            for (int j = 0; j < 12; j++) {
-                System.out.printf("%7.3f", d[i][j]);
-            }
-            System.out.println();
-        }
+        
+        printMatrix(d);
     }
 }
